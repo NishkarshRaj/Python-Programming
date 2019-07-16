1bbf2c4040d839afffcb55f8588a3ec378af37a4
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-16 08:11:42 +0530

    My first program in Python

---
+++ 1_Fundamental Python Programming/IDE.py
@@ -0,0 +1,6 @@
+4*5; ''' Works on normal IDE but not here but wont give any interpretation error'''
+41-31;
+print("Print Statement");
+print("It has inbuilt linefeed by default, we will learn how to override the default print configurations later");
+print("print","statement","has","inbuilt","spaces","within the comma separated strings");
+print("Output can also be written using " + "concatenation but spaces have to be then added accordingly");
