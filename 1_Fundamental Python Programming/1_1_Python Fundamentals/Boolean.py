81cef6c9632573f44f6cffd1686f63459adb27a2
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-18 19:10:02 +0530

    Boolean Data Type

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/Boolean.py
@@ -0,0 +1,14 @@
+true = True;
+false = False;
+true1 = "True";
+false1 = "False";
+true2 = true; ''' I was trying to check if keyword is case sensitive which it is not but here the above defined variable value is taken which is bool
+This technique can be used to define case insensitive keywords at start to py program.'''
+false2 = false;
+print(true,type(true));
+print(false,type(false));
+print(true1,type(true1));
+print(false1,type(false1));
+print(true2,type(true2));
+print(false2,type(false2));
+
