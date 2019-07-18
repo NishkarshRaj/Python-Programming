10312141c411f6f772e3c9e2b66c9ea48c6fb1c5
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-18 19:10:58 +0530

    Strings in python

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/String.py
@@ -0,0 +1,8 @@
+str1 = "Strings in double quotes";
+str2 = 'String in single quotes';
+str3 = "Quote's within quotes"
+str4 = 'I said,"I am a good boy", savvy?'
+print(str1);
+print(str2);
+print(str3);
+print(str4);
