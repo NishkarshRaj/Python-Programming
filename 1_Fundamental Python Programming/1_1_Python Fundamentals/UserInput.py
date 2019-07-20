256d276b872a0cf2a86bc5e37ad828d16ff81bbf
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-20 20:51:48 +0530

    Taking User input in Python

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/UserInput.py
@@ -0,0 +1,11 @@
+print("Taking User Input at Runtime using input() Method");
+print("Note: Default Input type is String");
+
+var = input();
+print(var);
+print(type(var));
+
+
+var1 = input("Enter a value: "); #Prompt message with input() method
+print(var1);
+print(type(var1));
