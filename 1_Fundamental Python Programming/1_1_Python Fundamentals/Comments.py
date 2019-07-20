25039399370e31bbe2d18c7d08ac09773ecd05cb
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-20 20:51:02 +0530

    Comments in Python

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/Comments.py
@@ -0,0 +1,19 @@
+print("Comments in Python")
+print("Single line comments basically done to explain the code line in parallel to the comment. It uses the symbol #"); #Statement for single line comment explanation
+print("Multiple line comments used for documentation. Symbol is Single quotes or double quotes");
+'''
+Multi
+Line
+comments
+using
+single
+quotes
+'''
+"""
+Multi
+Line
+Comments
+using
+Double
+Quotes.
+"""
