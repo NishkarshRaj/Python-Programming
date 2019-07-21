928c8cc17d69a7c712a3e135ac47e94cb6c27242
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-21 19:51:08 +0530

    Integer typecasted user input

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/IntegerInput.py
@@ -0,0 +1,5 @@
+print("Taking User Input and Typecasting it to Integer");
+str = input("Enter generic input: ");
+int1 = input("Enter an Integer: ");
+print(str + int1); #Concatenation of string and integer.
+print(int1 + str);
