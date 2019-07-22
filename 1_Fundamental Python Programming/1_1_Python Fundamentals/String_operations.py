c2d76300bcdb5ff0f1b1ba71db502dbc8440e9eb
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-22 17:31:46 +0530

    String with mathematical operators + and *

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/String_operations.py
@@ -0,0 +1,10 @@
+#print(41+"Nishkarsh");
+#print("Nishkarsh"+41);
+#print(1+4+"Nishkarsh"+6+7);
+'''Note: Concatenation not allowed for string and integers in print method'''
+
+print("Nishkarsh"*4);
+print(4*"Nishkarsh");
+'''String multiplication Works in both manner'''
+
+
