b3ae7db6ce2777fdde1810ce0204ae643e9f4e6d
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-22 18:13:33 +0530

    Bitwise Operators in Python

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/BitwiseOperators.py
@@ -0,0 +1,77 @@
+print("Bitwise Operators in Python");
+
+print("");
+print("");
+
+print("1) Bitwise & Operator (AND)");
+print("5&4 =",5&4);
+'''
+Logic: Column Wise, 1 if all 1 else 0.
+
+5   = 101
+4   = 100
+out = 100 = 4
+'''
+
+print("");
+print("");
+
+print("2) Bitwise | Operator (OR)");
+print("5|4 =",5|4);
+'''
+Logic: Column Wise, 1 if atleast 1 else 0.
+
+5   = 101
+4   = 100
+out = 101 = 5
+'''
+
+print("");
+print("");
+
+print("3) Bitwise ~ Operator (One's Complement)");
+print("~5",~5);
+'''
+Logic: Not all bits
+1 -> 0
+0 -> 1
+
+5  = 101
+~5 = 010 = 2
+'''
+
+print("");
+print("");
+
+print("4) Bitwise ^ Operator (XOR)");
+print("5^4 =",5^4);
+'''
+Logic: If odd number of 1 in column then 1 else 0
+
+5   = 101
+4   = 100
+out = 001 = 1
+'''
+
+print("");
+print("");
+
+print("5) Bitwise << Operator (Left Shift)");
+print("5<<4 =",5<<4);
+'''
+Left shift a<<b == a*(2**b)
+'''
+
+print("");
+print("");
+
+print("6) Bitwise >> Operator (Right Shift)");
+print("5>>4 =",50>>4);
+'''
+Right shift a>>b == a/(2**b)
+'''
+print("Note: Answer of Right Shift always in form of Floor Division //");
+
+print("");
+print("");
+
