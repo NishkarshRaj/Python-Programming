0bad3f92a9eab1ca2e8b0f1f5d6efad914db3402
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-22 17:37:08 +0530

    Variable Declaration in Python (Generic)

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/VariableDeclaration_Generic.py
@@ -0,0 +1,6 @@
+print("Declaring multiple variables in single line of code and assigning values of different data types");
+
+var1,var2,var3 = 1 + 2j, "Nishkarsh Raj Khare", True;
+print(var1);
+print(var2);
+print(var3);
