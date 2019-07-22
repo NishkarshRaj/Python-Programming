3faa64857d1cd305d9abac8cd837814c90ac76d1
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-22 17:51:50 +0530

    Important Arithmetic Operators in Python

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/ArithmeticOperator.py
@@ -0,0 +1,28 @@
+print("Important Arithmetic Operators");
+
+print("");
+print("");
+
+print("Division Operator");
+print("/ Operator gives the Quotient always in floating point number");
+print("10/2 =",10/2);
+print("10/4 =",10/4);
+
+print("% Operator gives the Remainder in decimal division where results are not in floating point but integers only");
+print("10%2 =",10%2);
+print("10%4 =",10%4);
+
+print("// Operator gives the Quotient corresponding Remainder as Integer only division");
+print("10//2 =",10%2);
+print("10//4 =",10%4);
+
+print("Note: Remainder is always a positive number!!!");
+print("-10//3 =",(-10//3));
+print("-10%3 =",(-10%3));
+
+print("");
+print("");
+
+print("Exponentiation Operator **");
+print("2**10 =",2**10);
+
