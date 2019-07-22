5e3d03db343ee6228ff0bc2f21ed89e28f83c87e
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-22 17:56:31 +0530

    Using Not Equals in different ways

---
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/RelationalOperator_NotEquals.py
@@ -0,0 +1,9 @@
+print("Different ways of using Not Equal Operator in Python");
+
+print("Way 1: Using !=");
+print("10!=4 is",10!=4);
+print("10!=10 is",10!=10);
+
+print("Way 2: Using <>");
+#print("10<>4 is",10 <> 4); Does not work??
+#print("10<>10 is",10 <> 10);
