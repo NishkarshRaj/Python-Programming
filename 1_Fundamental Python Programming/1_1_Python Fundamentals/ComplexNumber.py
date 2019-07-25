2c4b2c94f8909a0956fda4a8c986d3dbbd653a2c
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-25 19:43:23 +0530

    Complex number absolute value issue solved

--- 1_Fundamental Python Programming/1_1_Python Fundamentals/ComplexNumber.py
+++ 1_Fundamental Python Programming/1_1_Python Fundamentals/ComplexNumber.py
@@ -2,3 +2,7 @@ var = 3.4 + 4j; #Complex Number declaration
 print(var);
 print(var.real); #prints real part of complex number
 print(var.imag); #prints imaginary part of complex number
+print("The value of a complex number is defined by: ");
+print("sqrt(complex.real**2 + complex.imag**2)");
+print("Shortcut: abs(complex)");
+print(abs(var));
