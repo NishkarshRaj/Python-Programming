644a48a831a56c434941ef68fae37624365ad805
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-29 13:01:58 +0530

    Overriding print delimeter

---
+++ 1_Fundamental Python Programming/1_2_Control Structures/OverridePrintDelimiter.py
@@ -0,0 +1,13 @@
+print("Overriding default print statement");
+print("Using different delimiters with print");
+
+#Using space for delimeter
+for i in range(1,11):
+    print(i,end=" ");
+
+print("");
+
+#Using asterisk for delimeter
+for i in range(1,11):
+    print(i,end="*");
+    
