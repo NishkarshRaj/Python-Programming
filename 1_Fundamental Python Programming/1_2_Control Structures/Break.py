c30805c453139402515756553178fed96058b4d1
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-29 13:09:27 +0530

    Break Statement

---
+++ 1_Fundamental Python Programming/1_2_Control Structures/Break.py
@@ -0,0 +1,10 @@
+print("Break Statement in looping structures");
+
+i = 1;
+
+# infinite loop with break
+while(True):
+    i = i+1;
+    print("Nishkarsh Raj Khare");
+    if(i==5):
+        break;
