5a221859a710568cb8fe94aa264eb7e1561c820c
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-29 12:32:25 +0530

    For loop using range keyword

---
+++ 1_Fundamental Python Programming/1_2_Control Structures/For_range.py
@@ -0,0 +1,10 @@
+print("Generic For loop with range");
+
+''' Prints from start inclusive to end exclusive with +1 increment '''
+for i in range (0,101):
+    print(i);
+
+''' Prints from start inclusive to end exclusive with defined jump'''
+for i in range (0,101,2):
+    print(i);
+    
