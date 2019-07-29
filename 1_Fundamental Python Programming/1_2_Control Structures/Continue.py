aaa0a304a0dec0103eb4946d1ddcf4e5479f9707
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-29 13:13:37 +0530

    Continue statement

---
+++ 1_Fundamental Python Programming/1_2_Control Structures/Continue.py
@@ -0,0 +1,12 @@
+print("Continue Statement in Python");
+
+i = 1;
+
+while(True):
+    if(i==5):
+        i = i + 1;
+        continue;
+    if(i==10):
+        break;
+    print(i);
+    i = i + 1;
